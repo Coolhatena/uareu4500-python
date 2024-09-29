027e5766a611fb42f6addf36e056a860606297cd
Eduardo Rivera Geffroy <ingsc.geffroy@gmail.com>
2024-09-28 19:36:26 -0700

    First script version capable of accessing the fingerprint reader

--- uareu4500.py
+++ uareu4500.py
@@ -23,37 +23,46 @@ fingerprint_lib.compare_fmds.argtypes = [c_uint, POINTER(c_ubyte), c_uint, POINT
 
 # Ejemplo de llamada a las funciones desde Python
 def init_library():
-    return fingerprint_lib.init_library()
+	return fingerprint_lib.init_library()
 
 def search_devices():
-    device_name = ctypes.c_char_p()
-    result = fingerprint_lib.search_devices(ctypes.byref(device_name))
-    return result, device_name.value
+	device_name = ctypes.c_char_p()
+	result = fingerprint_lib.search_devices(ctypes.byref(device_name))
+	return result, device_name.value
 
 def open_device(device_name):
-    hReader = ctypes.c_void_p()
-    result = fingerprint_lib.open_device(device_name.encode('utf-8'), ctypes.byref(hReader))
-    return result, hReader
+	hReader = ctypes.c_void_p()
+	result = fingerprint_lib.open_device(device_name, ctypes.byref(hReader))
+	return result, hReader
 
 def capture_fid(hReader):
-    fid_size = ctypes.c_uint()
-    fid_data = ctypes.POINTER(c_ubyte)()
-    result = fingerprint_lib.capture_fid(hReader, ctypes.byref(fid_size), ctypes.byref(fid_data))
-    return result, fid_size.value, fid_data
+	fid_size = ctypes.c_uint()
+	fid_data = ctypes.POINTER(c_ubyte)()
+	result = fingerprint_lib.capture_fid(hReader, ctypes.byref(fid_size), ctypes.byref(fid_data))
+	return result, fid_size.value, fid_data
 
 def transform_fid_to_fmd(fid_size, fid_data):
-    fmd_size = ctypes.c_uint()
-    fmd_data = ctypes.POINTER(c_ubyte)()
-    result = fingerprint_lib.transform_fid_to_fmd(fid_size, fid_data, ctypes.byref(fmd_data), ctypes.byref(fmd_size))
-    return result, fmd_size.value, fmd_data
+	fmd_size = ctypes.c_uint()
+	fmd_data = ctypes.POINTER(c_ubyte)()
+	result = fingerprint_lib.transform_fid_to_fmd(fid_size, fid_data, ctypes.byref(fmd_data), ctypes.byref(fmd_size))
+	return result, fmd_size.value, fmd_data
 
 def compare_fmds(fmd1_size, fmd1_data, fmd2_size, fmd2_data):
-    score = ctypes.c_uint()
-    result = fingerprint_lib.compare_fmds(fmd1_size, fmd1_data, fmd2_size, fmd2_data, ctypes.byref(score))
-    return result, score.value
+	score = ctypes.c_uint()
+	result = fingerprint_lib.compare_fmds(fmd1_size, fmd1_data, fmd2_size, fmd2_data, ctypes.byref(score))
+	return result, score.value
 
 # Ejemplo de uso
 if __name__ == "__main__":
-    print("Inicializando la librería...")
-    result = init_library()
-    print("Resultado:", result)
+	# print("Inicializando la librería...")
+	# result = init_library()
+	# print("Resultado:", result)
+	
+	# print("Abriendo dispositivos...")
+	# result, device_name = search_devices()
+	# print("Resultado:", result)
+	# print("Nombre del dispositivo", device_name)
+
+	# result, hReader = open_device(device_name.decode('utf-8'))
+	# print("Resultado:", result)
+	fingerprint_lib.main()
